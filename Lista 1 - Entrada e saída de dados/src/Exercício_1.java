66eee8de1c475e416e943d2330de0a67cd1cc486
LaysllaOreti <laysllaoreti2006@gmail.com>
2025-09-12 11:05:33 -0300

    Desenvolvimento da Lista 1 no dia 12_09_2025

---
+++ Lista 1 - Entrada e saída de dados/src/Exercício_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercício_1 {
+    public static void main(String[] args) {
+        System.out.println("Dados de Cadastro - Vagas Bosch");
+
+        // Para ler todos os dados
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite o seu primeiro nome: ");
+        String nome = read.nextLine();
+    }
+}
